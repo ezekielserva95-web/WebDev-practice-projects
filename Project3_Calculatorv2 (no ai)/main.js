eb75f8bbef4f9b5fd22f1fcaff993059bfca7089
ezekielserva95-web <ezekielserva95@gmail.com>
2026-02-18 02:23:24 +0800

    Adding The rest of my projects

---
+++ Project3_Calculatorv2 (no ai)/main.js
@@ -0,0 +1,13 @@
+const display = document.getElementById("display");
+
+function appearToDisplay(input) {
+  display.value += input;
+}
+
+function clearDisplay() {
+  display.value = " ";
+}
+
+function calculate() {
+  display.value = eval(display.value);
+}
